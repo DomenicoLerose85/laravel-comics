6e3d4b26b7aed807f9dd38d513da3c006b267126
DomenicoLerose85 <domenicolerose85@libero.it>
2022-06-10 18:01:22 +0200

    comics_imgs_printed_on_home

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -8,5 +8,15 @@ DC Comics - Home
 
 @section('mainContent')
    <h1>Home</h1> 
-   @dump($fumetti)
+   {{-- @dump($fumetti) --}}
+   <div class="container">
+       <div class="row">
+           @foreach ($fumetti  as $fumetti)
+            <div class="col">
+                <img src="{{$fumetti['thumb']}}" alt="{{$fumetti['thumb']}}">
+            </div>
+               
+           @endforeach
+       </div>
+   </div>
 @endsection
